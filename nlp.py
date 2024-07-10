28b94bdcb9e381f0f60ee51bf6584a23aa49ef76
AlladaJyothi <alladajyothi12@gmail.com>
2024-07-10 16:07:42 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -12,12 +12,13 @@ name=st.title('Email Spam and Ham Prediction')
 model=pickle.load(open("model.pkl",'rb'))
 bow=pickle.load(open("bow.pkl",'rb'))
 
-email = st.text_input('Enter the Email:')
-st.button("Submit")
-data = bow.transform([email]).toarray() 
-spam_ham = model.predict(data)[0]
-spam_ham
 
-if spam_ham=='spam':
-    st.image("spam image.PNG",width=200)
+email = st.text_input('Enter the Email:')
+    if st.button("Submit"):
+    data = bow.transform([email]).toarray() 
+    spam_ham = model.predict(data)[0]
+    spam_ham
+
+    if spam_ham=='spam':
+        st.image(r"C:\Users\DELL\Pictures\spam image.PNG")
 
